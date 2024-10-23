cbb92a297be0ed1f5f29c22cfcbad4bbb88c392d
Victor <47982205+VictorCFerreira@users.noreply.github.com>
2024-10-23 20:21:04 -0300

    Ajustes 23-10-2024

--- app/src/main/java/com/example/registroturistico/MainActivity.kt
+++ app/src/main/java/com/example/registroturistico/MainActivity.kt
@@ -2,14 +2,18 @@ package com.example.registroturistico
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+
+private const val LOCATION_PERMISSION_REQUEST_CODE = 1
 
 class MainActivity : AppCompatActivity(), LocationListener {
 
@@ -18,14 +22,26 @@ class MainActivity : AppCompatActivity(), LocationListener {
 
     private lateinit var locationManager : LocationManager
 
+    private lateinit var btnGoToMaps: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvLatitude = findViewById( R.id.tvLatitude )
-        tvLongitude = findViewById( R.id.tvLongitude )
+        tvLatitude = findViewById( R.id.etLatitude )
+        tvLongitude = findViewById( R.id.etLongitude )
 
         locationManager = getSystemService( Context.LOCATION_SERVICE ) as LocationManager
+
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
 
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -35,18 +51,18 @@ class MainActivity : AppCompatActivity(), LocationListener {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
+            return;
+        } else {
+            locationManager.requestLocationUpdates( LocationManager.NETWORK_PROVIDER,
+                0, 0f, this );
         }
 
-        locationManager.requestLocationUpdates( LocationManager.NETWORK_PROVIDER,
-            0, 0f, this )
+        btnGoToMaps = findViewById(R.id.btnGoToMaps)
+
+        btnGoToMaps.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
